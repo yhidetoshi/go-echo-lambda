ecb29efbe7ab730621cd79766dbe81991b3728d6
yhidetoshi <hyajima.1014@gmail.com>
2022-10-15 14:48:23 +0900

    Add APIGW http api version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # go-echo-lambda
 APIGateway + Lambda(Go Echo) + ServerlessFramework
+
+- デプロイコマンド
+```sh
+$ GOARCH=amd64 GOOS=linux go build "-ldflags=-s -w" ./main.go
+$ sls deploy --param="account_id=${AWS_ACCOUNT_ID}" --param="allow_id=X.X.X.X/32"
+```
--- main.go
+++ main.go
@@ -14,7 +14,8 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-var echoLambda *echoadapter.EchoLambda
+//var echoLambda *echoadapter.EchoLambda   // APIGW(REST)
+var echoLambda *echoadapter.EchoLambdaV2 // APIGW(HTTP)
 
 func init() {
 	log.Printf("echo cold start")
@@ -24,13 +25,15 @@ func init() {
 	e.Use(auth.BasicAuth())
 	e.GET("/api/healthcheck", healthcheck.Healthcheck)
 
-	echoLambda = echoadapter.New(e)
+	//echoLambda = echoadapter.New(e) // APIGW(REST)
+	echoLambda = echoadapter.NewV2(e) // APIGW(HTTP)
 }
 
 func main() {
 	lambda.Start(Handler)
 }
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+//func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) { // APIGW(REST)
+func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) { // APIGW(HTTP)
 	return echoLambda.ProxyWithContext(ctx, req)
 }
